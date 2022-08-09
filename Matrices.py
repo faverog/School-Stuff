3cb84c77b317152d1beba3b40f33674d08302ff7
Uygur Tepe <uygur.t10@gmail.com>
2022-08-09 15:34:31 -0400

    updated functionality of matricies

--- Matrices.py
+++ Matrices.py
@@ -1,29 +1,35 @@
-from sympy import init_printing
-from sympy import Matrix
+from sympy import init_printing, pprint
+from sympy import Matrix, eye, zeros
 
 init_printing()
 
 # Import needed variables here
 from sympy.abc import K, s, k, x, y, J, D, b, l, L, R
 
-# Define matrix (Exponents are denoted by '**')
-A = Matrix([[s, s+1], [2*s+1, s/s+3]])
+# Define matricies (Exponents are denoted by '**')
+A = Matrix([[-2, 2, 1], [2, -2, 0], [-1, 0, 0]])
+B = Matrix([[1], [-1], [1]])
+C = Matrix([[1, -1, 0]])
+D = Matrix([0])
+
+#Define sI-A matrix
+sI = s*eye(A.rows)
+sI_minus_A = sI - A
+pprint(sI_minus_A)
 
 # Compute adjugate matrix
-adj_A = A.adjugate()
-print(adj_A)
+adj_sI_minus_A = sI_minus_A.adjugate()
+pprint(adj_sI_minus_A)
 
 # Compute determinant of the matrix
-det_A = A.det()
-print(det_A)
+det_sI_minus_A = sI_minus_A.det()
+pprint(det_sI_minus_A)
 
 # Compute inverse matrix
-inv_A = A.inv()
-print(inv_A)
+inv_sI_minus_A = sI_minus_A.inv()
+pprint(inv_sI_minus_A)
 
-
-
-
-
-
-
+#Compute Transfer Function Model (Gs)
+#this method of calculating allows sympy to simplify the numerator of the TF
+Gs = D + (C*adj_sI_minus_A*B)/det_sI_minus_A
+pprint(Gs)
